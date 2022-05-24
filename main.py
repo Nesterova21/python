290209a4d622a88a79e13aa400a9c5507742fd36
Nesterova <104452104+Nesterova21@users.noreply.github.com>
2022-05-24 20:12:21 +0000

    задание 123

--- main.py
+++ main.py
@@ -1,40 +1,12 @@
-cubes = [x**3 for x in range (100) if  x%2 != 0 ]
-print(cubes)
-some_of_numbs = 0
-some_of_numbs_list=[]
-
-for i in range(len(cubes)):
-  my_str = str(cubes[i])
-  my_list = list(my_str)   
-for i in range(len(my_list)):
-  my_list[i] = int(my_list[i])    
-for i in range(len(my_list)):
-  some_of_numbs = some_of_numbs + my_list[i]
-if some_of_numbs % 7 == 0:
-        print(some_of_numbs)
-some_of_numbs_list.append(some_of_numbs)
-print('Числа, которые делятся на 7 - ', some_of_numbs_list)
-
-cubes = [(x**3)+17 for x in range(100) if x%2 == 0]
-print(cubes)
-some_of_numbs = 0
-some_of_numbs_list_with_numbers =[]
-
-for i in range(len(cubes)):
-    my_str = str(cubes[i])
-    my_list = list(my_str)
-    for i in range(len(my_list)):
-        my_list[i] = int(my_list[i])
-
-for i in range(len(my_list)):
-        some_of_numbs = some_of_numbs + my_list[i]
-
-if some_of_numbs % 7 == 0:
-        print(some_of_numbs)
-        some_of_numbs_list_with_numbers.append(some_of_numbs)
-
-if some_of_numbs % 7 == 0:
-        print(some_of_numbs)
-        some_of_numbs_list_with_numbers.append(some_of_numbs)
-
-print('Числа, которые делятся на 7 - ',some_of_numbs_list_with_numbers)
+for i in range(100):
+    new_str=str(i+1)
+    new_list = list(new_str)
+    if int(new_list[-1])==1 and i+1!=11:
+        print('{} процент'.format(i + 1))
+    elif int(new_list[-1])>1 and int(new_list[-1])<= 4:
+        if  i+1> 10 and i+1<= 14:
+            print('{} процентов'.format(i + 1))
+        else:
+            print('{} процента'.format(i + 1))
+    else:
+        print('{} процентов'.format(i + 1))
